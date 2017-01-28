5a57da1384eeb5504eebf981bee1748467529697
Dale Jung <dale@dalejung.com>
2017-01-28 01:32:57 -0500

    testing scripts

--- app/app.py
+++ app/app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
 def hello_world(path):
-    return path + ' :: ' + str(datetime.datetime.now())
+    return path + ' :: ' + str(datetime.datetime.now()) + "\n"
 
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0')
---
+++ test_bans.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+docker-compose exec varnish varnishadm backend.set_health default healthy
+
+docker-compose exec varnish varnishadm backend.list
+
+# prime caches
+echo -e "\nWarm Caches:\n"
+docker-compose exec varnish curl http://127.0.0.1:80/dale
+docker-compose exec varnish curl http://127.0.0.1:80/dale2
+docker-compose exec varnish curl http://127.0.0.1:80/bob
+docker-compose exec varnish curl http://127.0.0.1:80/bob2
+
+docker-compose exec varnish varnishadm ban "req.url ~ /bob*"
+
+docker-compose exec varnish varnishadm backend.set_health default sick
+docker-compose exec varnish varnishadm backend.list
+
+docker-compose exec varnish varnishadm ban.list
+
+# try agian
+echo -e "\nTry Again:\n"
+docker-compose exec varnish curl http://127.0.0.1:80/dale
+docker-compose exec varnish curl http://127.0.0.1:80/bob
